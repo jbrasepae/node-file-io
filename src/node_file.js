8616de1a0436812be51557dfa95c18d2568b1fc5
Jeanette <jbrasepae@gmail.com>
2020-08-11 16:26:16 +0200

    structure created

---
+++ src/node_file.js
@@ -0,0 +1,15 @@
+class Visitors{
+    constructor(fullName, age, dateOfVisit, timeOfVisit, comments, assistedBy){
+        this.fullName = fullName;
+        this.age = age;
+        this.dateOfVisit = dateOfVisit;
+        this.timeOfVisit = timeOfVisit;
+        this.comments = comments;
+        this.assistedBy = assistedBy;
+    }
+    save(){
+        var fs = require('fs')
+
+    }
+    load()
+}
