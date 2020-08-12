24cd60128192bc5db269385f7a359e2838a6af23
Jeanette <jbrasepae@gmail.com>
2020-08-12 16:04:21 +0200

    added code

--- src/node_file.js
+++ src/node_file.js
@@ -1,4 +1,6 @@
-class Visitors{
+let fs = require('fs')
+
+class Visitor{
     constructor(fullName, age, dateOfVisit, timeOfVisit, comments, assistedBy){
         this.fullName = fullName;
         this.age = age;
@@ -7,9 +9,43 @@ class Visitors{
         this.comments = comments;
         this.assistedBy = assistedBy;
     }
-    save(){
-        var fs = require('fs')
 
+    save(){
+        let nameOfFiles = this;
+        let dataOfFiles = JSON.stringify(nameOfFiles, "\n", 4);
+        var theirFullNames = this.fullName.split(" ").join("_");
+
+        fs.writeFile(`visitor_${theirFullNames}.json`, dataOfFiles, (err) =>{
+            if(err) {
+                throw(Error + 'data was not saved')
+            }
+            else{
+            console.log("data is saved")
+            }
+        });
+        
     }
-    load()
 }
+
+function load(name){
+      var fullName = name.split(" ").join("_");
+     fs.readFile(`./visitor_${fullName}.json`, 'utf8', (err, jsonData) => {
+        if (err) {
+                throw err;
+            }
+            console.log("data loaded", jsonData); 
+        })
+
+}
+
+let alice = new Visitor('alice cooper', 20, '15/03/2020', '11h00', 'great service', 'Boitumelo');
+alice.save();
+load("alice cooper")
+
+let bob = new Visitor('bob marley', 18, '10/09/2019', '09H00', 'how to apply', 'Jeanette');
+bob.save();
+load("bob marley");
+
+let charlie = new Visitor('charley sheen', 22, '20/04/2019', '10h30', 'which department does umuzi offer', 'Bontle');
+charlie.save();
+load("charley sheen")
