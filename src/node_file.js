3f6242a95d4189ad350fb3d49e29e9f760c33761
Jeanette <jbrasepae@gmail.com>
2020-08-13 22:53:12 +0200

    changes made

--- src/node_file.js
+++ src/node_file.js
@@ -11,30 +11,28 @@ class Visitor{
     }
 
     save(){
-
         let theirFullNames = this.fullName.toLowerCase().split(" ").join("_");
         fs.writeFile(`visitor_${theirFullNames}.json`, JSON.stringify(this, "\n", 4), (err) =>{
             if(err) {
                 throw Error('file was not saved');
             }
-            else{
-            console.log("file is saved");
-            }
+            // else{
+            // console.log("file is saved");
+            // }
         });
         return "file is saved"     
     }
 
      load(FullNames){
         let theirFullNames = FullNames.toLowerCase().split(" ").join("_"); 
-        fs.readFile(`./visitor_${theirFullNames}.json`, 'utf8', (err, jsonData) => {
+        fs.readFile(`./visitor_${theirFullNames}.json`, 'utf8', (err, data) => {
            if (err) {
                    throw Error("invalid data");
                }
                else{
-               console.log( jsonData);
+               console.log(data);
                } 
         });
-        //return (jsonData);
     }
 }
 
@@ -42,11 +40,11 @@ let alice = new Visitor('Alice Cooper', 20, '15/03/2020', '11h00', 'great servic
 alice.save();
 alice.load("Alice Cooper")
 
-let bob = new Visitor('Bob Marley', 18, '10/09/2019', '09H00', 'how to apply', 'Jeanette');
+let bob = new Visitor('Bob Marley', 18, '10/09/2019', '09H00', 'keep up the good work', 'Jeanette');
 bob.save();
 bob.load("Bob Marley");
 
-let charlie = new Visitor('Charley Sheen', 22, '20/04/2019', '10h30', 'which department does umuzi offer', 'Bontle');
+let charlie = new Visitor('Charley Sheen', 22, '20/04/2019', '10h30', 'amazing work', 'Bontle');
 charlie.save();
 charlie.load("Charley Sheen")
 
